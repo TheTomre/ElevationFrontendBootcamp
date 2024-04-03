167514e60cd2ad639b51595e6e64700bd5247f98
thetomre <shnkirill@gmail.com>
2024-04-03 15:43:41 +0300

    fixed bugs added logic and some styles

--- assignments/assignment1/src/js/main.js
+++ assignments/assignment1/src/js/main.js
@@ -1,33 +1,51 @@
-import { contacts } from '../data/contacts.js';
+import { contacts } from "../data/contacts.js";
 
-const inputEl = document.querySelector('[data-input]');
-const btnEl = document.querySelector('button');
-const searchResult = document.querySelector('[data-search-result]')
-debugger
+const inputEl = document.querySelector("[data-input]");
+const btnEl = document.querySelector("button");
+const alert = document.querySelector("[data-alert]");
+const searchResult = document.querySelector("[data-search-list]");
 
-
-function isNameOrPhone (data) {
-    return /[0-9]/.test(data)
-
-    
+function isNameOrPhone(data) {
+    return /[0-9]/.test(data);
 }
 
-btnEl.addEventListener('click', () => {
-    let result;
-    if (isNameOrPhone(inputEl.value)) {
-        result = contacts.filter(contact => contact.phone.includes(inputEl.value) || contact.cell.includes(inputEl.value))
+btnEl.addEventListener("click", () => {
+    if (inputEl.value != "") {
+        alert.innerHTML = null;
+        searchResult.innerHTML = null;
+        let result = null;
+        const query = inputEl.value.toLowerCase();
 
-    } else {
-        result = contacts.filter(contact => contact.name.first.includes(inputEl.value) || contact.name.last.includes(inputEl.value))
+        if (isNameOrPhone(query)) {
+            result = contacts.filter(
+                (contact) =>
+                    contact.phone.toLocaleLowerCase().includes(query) ||
+                    contact.cell.toLocaleLowerCase().includes(query)
+            );
+        } else {
+            result = contacts.filter(
+                (contact) =>
+                    contact.name.first.toLocaleLowerCase().includes(query) ||
+                    contact.name.last.toLocaleLowerCase().includes(query)
+            );
+        }
+
+        if (result.length == 0) {
+            searchResult.innerHTML =
+                "<h5> No results found for your search </h5>";
+        } else {
+            result.forEach((element) => {
+                const liEl = document.createElement("li");
+                liEl.innerText =
+                    element.name.first +
+                    " " +
+                    element.name.last +
+                    " Phone: " +
+                    element.phone +
+                    " Cell: " +
+                    element.cell;
+                searchResult.appendChild(liEl);
+            });
+        }
     }
-    
-
-    result.forEach(element => {
-        const liEl = document.createElement('li');
-        liEl.innerText = element.name.first + " " + element.name.last + " phone: " + element.phone + " cell: " + element.cell
-        searchResult.appendChild(liEl);
-    });
-
-})
-
-
+});
